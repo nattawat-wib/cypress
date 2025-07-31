e20c3710d7b8c87a9e692e9409b2a1b7f33aa56c
nattawat_sit <nattawat@sense-infotech.com>
2025-07-31 08:51:49 +0700

    feat: screen capture

--- cypress/e2e/0-gsb/LEGSCST200/search-by-cif.cy.js
+++ cypress/e2e/0-gsb/LEGSCST200/search-by-cif.cy.js
@@ -8,8 +8,10 @@ describe('search by cif', () => {
     it('should search successfully with valid CIF', () => {
         // Visit LEGSCST200 page
         cy.visit('http://sitdev.dyndns.org:9185/#/LEGSCST200')
+        cy.screenshot();
 
         cy.get('input[name="cifNumber"]').type('750590586')
+        cy.screenshot();
 
         // search button
         cy.get('button[type="submit"][aria-describedby=":ru:"].btn.btn-light-info').click()
@@ -17,10 +19,19 @@ describe('search by cif', () => {
         // view detail button
         cy.get('button[aria-describedby=":r15:"]', { timeout: 30000 }).click()
 
-        cy.get('input[placeholder="ประเภทลูกค้า"]')
+        cy
+            .get('input[placeholder="ประเภทลูกค้า"]')
+            // .should('have.value', 'นิติบุคคล')
             .should('have.value', 'บุคคลธรรมดา')
             .then(() => {
-                cy.log(' ===== ค้นหาบุคคลธรรมดา Test Result Successfully completed ====')
+                // cy.get('div[aria-labelledby=":r18:"]').screenshot( );
+                cy.log(' ===== successfully ลูกค้าเป็นบุคคลธรรดา ====');
+            }, (error) => {
+                cy.log(' ===== fail ลูกค้าเป็นบุคคลธรรดา ====')
+                throw error;
             })
+
+        // cy.get('div.ant-modal-content', { timeout: 6000 }).screenshot();
+        cy.get('div[aria-labelledby=":r18:"]', { timeout: 6000 }).screenshot();
     })
 })
